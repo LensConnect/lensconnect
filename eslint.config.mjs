f32a98e9d7112f6f6201d0d1cde019c89029803f
izzymony <ojehonmonisrael008@gmail.com>
2026-09-21 11:19:24 +0100

    fix: correct import statement for ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import eslintConfig from "@eslint/eslintrc";
-const { FlatCompat } = eslintConfig;
+import eslintrc from "@eslint/eslintrc";
+const { FlatCompat } = eslintrc;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
